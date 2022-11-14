83f11c3cba080dab2f6b37868a7fe235f7f91ced
Zhenyu Liu <z.liu.235@westcliff.edu>
2022-11-14 12:42:39 -0800

    Update docker file

--- functions/functions.php
+++ functions/functions.php
@@ -1,9 +1,19 @@
 <?php
 // Establish Connection to Database
+define("DB_HOST", "db");
+define("DB_USER", "mysql_user");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "mysqldb");
+
 function connect() {
     static $conn;
     if ($conn === NULL){ 
-        $conn = mysqli_connect('localhost','root','','socialnetwork');
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, 9906);
+        if ($conn->connect_error) {
+            die("connection failed: " . $conn->connect_error);
+        } else {
+            echo "Connected to mysql server successfully!";
+        }
     }
     return $conn;
 }
